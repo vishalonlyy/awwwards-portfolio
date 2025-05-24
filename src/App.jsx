8a2620bc43efd4c0e75a9c754432b0ecdc061760
AliR <AliR@ALI>
2025-05-24 05:56:39 +0330

    Modify Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./sections/Navbar";
 
 const App = () => {
   return (
-    <div className="relative w-screen min-h-screen overflow-x-hidden">
+    <div className="relative w-screen min-h-screen overflow-x-auto">
       <Navbar />
       <section id="home" className="min-h-screen" />
       <section id="services" className="min-h-screen bg-amber-900" />
