f14ecaba0d7ca4abd8232e4f6ae35a72018e05ad
AliR <AliR@ALI>
2025-06-01 19:21:37 +0330

    Add ServiceSummary.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
+import ServiceSummary from "./sections/ServiceSummary";
 
 const App = () => {
   return (
     <div className="relative w-screen min-h-screen overflow-x-auto">
       <Navbar />
       <Hero />
+      <ServiceSummary />
     </div>
   );
 };
