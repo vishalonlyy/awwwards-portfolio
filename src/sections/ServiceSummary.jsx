1336975172f4406e5837b17d902f38e59f3e7b53
AliR <AliR@ALI>
2025-06-02 07:25:49 +0330

    Modify ServiceSummay.jsx to fix overflow-hidden

--- src/sections/ServiceSummary.jsx
+++ src/sections/ServiceSummary.jsx
@@ -34,7 +34,7 @@ const ServiceSummary = () => {
     });
   });
   return (
-    <section className="mt-20 overflo-hidden font-light leading-snug text-center mb-42 contact-text-responsive">
+    <section className="mt-20 overflow-hidden font-light leading-snug text-center mb-42 contact-text-responsive">
       <div id="title-service-1">
         <p>Architucture</p>
       </div>
