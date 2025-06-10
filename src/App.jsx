80e13c29d0e9d21ca76c51165a5c91054c640a06
AliR <AliR@ALI>
2025-06-10 08:52:59 +0330

    Add Contact.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import ServiceSummary from "./sections/ServiceSummary";
@@ -7,20 +7,48 @@ import ReactLenis from "lenis/react";
 import About from "./sections/About";
 import Works from "./sections/Works";
 import ContactSummary from "./sections/ContactSummary";
+import Contact from "./sections/Contact";
+import { useProgress } from "@react-three/drei";
 
 const App = () => {
+  const { progress } = useProgress();
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    if (progress === 100) {
+      setIsReady(true);
+    }
+  }, [progress]);
+
   return (
     <ReactLenis root className="relative w-screen min-h-screen overflow-x-auto">
-      <Navbar />
-      <Hero />
-      <ServiceSummary />
-      <Services />
-      <About />
-      <Works />
-      <ContactSummary />
-      <section className="h-screen" />
-      <section className="h-screen" />
-      <section className="h-screen" />
+      {false && (
+        <div className="fixed inset-0 z-[999] flex flex-col items-center justify-center bg-black text-white transition-opacity duration-700 font-light">
+          <p className="mb-4 text-xl tracking-widest animate-pulse">
+            Loading {Math.floor(progress)}%
+          </p>
+          <div className="relative h-1 overflow-hidden rounded w-60 bg-white/20">
+            <div
+              className="absolute top-0 left-0 h-full transition-all duration-300 bg-white"
+              style={{ width: `${progress}%` }}
+            ></div>
+          </div>
+        </div>
+      )}
+      <div
+        className={`${
+          isReady ? "opacity-100" : "opacity-0"
+        } transition-opacity duration-1000`}
+      >
+        <Navbar />
+        <Hero />
+        <ServiceSummary />
+        <Services />
+        <About />
+        <Works />
+        <ContactSummary />
+        <Contact />
+      </div>
     </ReactLenis>
   );
 };
